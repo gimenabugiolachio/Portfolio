7f9a80626661c9e677b939d4c4b629b4f420b1a3
gimenabugiolachio <gimena.bugiolachio@mail.com>
2022-11-14 01:23:09 -0300

    Creamos el navbar y unos componentes más

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './Portfolio/inicio/inicio.component';
+import { NavbarComponent } from './Portfolio/navbar/navbar.component';
+import { PortfolioComponent } from './Portfolio/portfolio/portfolio.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavbarComponent,
+    PortfolioComponent
+
   ],
   imports: [
     BrowserModule,
